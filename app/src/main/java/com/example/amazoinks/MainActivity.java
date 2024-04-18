7180617eca6885212ef3e1c856d6d9d77f948c73
tr3nt-tayl0r <tretaylor@csumb.edu>
2024-04-18 15:58:12 -0700

    created login screen and signin/signup screen

--- app/src/main/java/com/example/amazoinks/MainActivity.java
+++ app/src/main/java/com/example/amazoinks/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.amazoinks;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,7 @@ import com.example.amazoinks.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "DTT_AMAZOINKS";
+    private static final String MAIN_ACTIVITY_USER_ID = "com.example.amazoinks.MAIN_ACTIVITY_USER_ID";
 
     private ActivityMainBinding binding;
     private AppRepository repository;
@@ -23,9 +26,28 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        loginUser();
+
+        if(loggedInUser == -1){
+            Intent intent = LoginActivity.loginIntentFactory(getApplicationContext());
+            startActivity(intent);
+        }
+
         repository = AppRepository.getRepository(getApplication());
+        assert repository != null;
         repository.invokeDB();
         setContentView(R.layout.activity_main);
 
+    }
+
+    private void loginUser() {
+        //TODO: create login method
+        loggedInUser = getIntent().getIntExtra(MAIN_ACTIVITY_USER_ID, -1);
+    }
+
+    static Intent mainActivityIntentFactory(Context context, int userId){
+        Intent intent = new Intent(context, MainActivity.class);
+        intent.putExtra(MAIN_ACTIVITY_USER_ID, userId);
+        return intent;
     }
 }
