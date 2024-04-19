b363c44361d87d84fadd07383d2d3e980a94a04e
tr3nt-tayl0r <tretaylor@csumb.edu>
2024-04-19 08:26:46 -0700

    created login screen and signin/signup screen, need to move sign-in/sign-up from MainActivity to its own Activity, so that LandingPage can be on MainActivity

--- app/src/main/java/com/example/amazoinks/LoginActivity.java
+++ app/src/main/java/com/example/amazoinks/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.amazoinks;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,7 +19,17 @@ public class LoginActivity extends AppCompatActivity {
         binding = ActivityLoginBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = MainActivity.mainActivityIntentFactory(getApplicationContext(), 0);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 
 
     static Intent loginIntentFactory(Context context){
